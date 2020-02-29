1abe37561fe65e4c4fced94cada0ea3a72008fb9
Santiago Zuluaga Hernandez <satiagozh1998@gmail.com>
2020-02-29 13:03:44 -0500

    Guardando cambios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,20 @@
 
 <script>
 export default {
+  data(){
+    return{
 
+    }
+  },
+  created(){
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=eac810b6ade616ce25d01396797173f0&sort_by=popularity.desc&region=US&include_video=true&page=1')
+    .then(res => res.json())
+    .then(res => console.log(res))
+
+    fetch('https://api.themoviedb.org/3/movie/454626/videos?api_key=eac810b6ade616ce25d01396797173f0&language=en-US')
+    .then(res => res.json())
+    .then(res => console.log(res))
+  }
 }
 </script>
 
